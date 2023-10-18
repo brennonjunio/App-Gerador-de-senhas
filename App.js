1f7abaf29964be2739c253bbec751afaaeb1fac7
Brennon Junio <juniobrennon@gmail.com>
2023-10-18 03:00:58 -0400

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import { useState } from "react";
+import Slider from "@react-native-community/slider";
+import { ModalPass } from "./src/components/modal";
 export default function App() {
+  const [size, SetSize] = useState(8);
+  const [pass, setPass] = useState("");
+  const [openModal, setOpenMOdal] = useState(false);
+  let charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+  function generatePass() {
+    let password = "";
+    for (let i = 0, n = charset.length; i < size; i++) {
+      password += charset.charAt(Math.floor(Math.random() * n));
+    }
+    setPass(password);
+    console.log(password);
+    setOpenMOdal(true);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.logo}
+        source={require("../novoapp/src/shared/logo.png")}
+      />
+      <Text style={styles.title}>{size} caracteres</Text>
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor="#ff0000"
+          value={size}
+          onValueChange={(value) => SetSize(value.toFixed(0))}
+        ></Slider>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.buttonText}>Gerar Senha</Text>
+      </TouchableOpacity>
+      <Modal visible={openModal} animationType="fade" transparent={true}>
+        <ModalPass password={pass} handleClose={() => setOpenMOdal(false)} />
+      </Modal>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F3F3FF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    marginBottom: 60,
+  },
+  area: {
+    marginTop: 14,
+    marginBottom: 14,
+    width: "80%",
+    backgroundColor: "#FFF",
+    borderRadius: 99,
+    padding: 6,
+  },
+  button: {
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 99,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
---
+++ src/components/modal/index.js
@@ -0,0 +1,85 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
+
+export function ModalPass({ password, handleClose }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Senha Gerada</Text>
+        <Pressable style={styles.innerPass}>
+          <Text style={styles.text}>{password}</Text>
+        </Pressable>
+        <View style={styles.buttonArea}>
+          <TouchableOpacity style={[styles.button, styles.buttonSave]}>
+            <Text style={[styles.buttonSaveText, styles]}>Salvar Senha</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleClose}>
+            <Text style={styles.buttonText}> Voltar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgba(24, 24, 24, 0.6)",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    backgroundColor: "#FFF",
+    width: "85%",
+    paddingTop: 24,
+    paddingBottom: 24,
+    alignItems: "center",
+    alignContent: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#000",
+    marginBottom: 24,
+  },
+  innerPass: {
+    backgroundColor: "#0e0e0e",
+    width: "90%",
+    padding: 14,
+    borderRadius: 8,
+  },
+  text: {
+    color: "#FFF",
+    textAlign: "center",
+  },
+  buttonArea: {
+    color: "#FFF",
+    flexDirection: "row",
+    width: "90%",
+    marginTop: 8,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 10,
+    padding: 8,
+  },
+  buttonSave: {
+    backgroundColor: "#392DE9",
+    borderRadius: 99,
+  },
+  buttonSaveText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+
+  buttonText: {},
+});
