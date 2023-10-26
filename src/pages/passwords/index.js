638a0a2f2f4f4c82a1e2ee500152488dd10b14f4
Brennon Junio <juniobrennon@gmail.com>
2023-10-25 22:26:50 -0400

    salvando progresso

---
+++ src/pages/passwords/index.js
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export function Passwords() {
+  return (
+    <View>
+      <Text>Minhas Senhas</Text>
+    </View>
+  );
+}
+const style = StyleSheet.create({});
---
+++ src/routes.js
@@ -0,0 +1,3 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {Home} from './pages/home/index'
+import {Pass} from './pages/passwords/index'
